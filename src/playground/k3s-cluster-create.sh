5a297d3a55b59d2b3700eafa636a39990ce3c5b2
Lyle Dodge <lyledodgegh@users.noreply.github.com>
2025-03-01 08:39:45 -0500

    reorganized into cloud-inits folder

--- src/playground/k3s-cluster-create.sh
+++ src/playground/k3s-cluster-create.sh
@@ -1,5 +1,12 @@
-# launch via multipass
-multipass launch --name node1 -d 80G -m 8G -c 4 --cloud-init node1-cloud-init.yaml
-multipass launch --name node2 -d 80G -m 8G -c 4 --cloud-init node2-cloud-init.yaml
-multipass launch --name node3 -d 80G -m 8G -c 4 --cloud-init node3-cloud-init.yaml
+# variables
+TOKEN=$(uuidgen)
 
+# launch via multipass
+multipass launch --name node1 -d 80G -m 8G -c 4 --cloud-init cloud-inits/aspnetcore-runtime.yaml
+multipass launch --name node2 -d 80G -m 8G -c 4 --cloud-init cloud-inits/aspnetcore-runtime.yaml
+multipass launch --name node3 -d 80G -m 8G -c 4 --cloud-init cloud-inits/aspnetcore-runtime.yaml
+
+# install k3s
+multipass exec node1 -- curl -sfL https://get.k3s.io | K3S_TOKEN=$TOKEN sh -
+multipass exec node2 -- curl -sfL https://get.k3s.io | K3S_URL=https://node1:6443 K3S_TOKEN=$TOKEN sh -
+multipass exec node3 -- curl -sfL https://get.k3s.io | K3S_URL=https://node1:6443 K3S_TOKEN=$TOKEN sh -
---
+++ src/playground/k3s-examples.sh
@@ -0,0 +1,2 @@
+
+multipass launch --name node1 -d 80G -m 8G -c 4 --cloud-init cloud-inits/aspnetcore-runtime.yaml
