95152c0fb9c187fb51dd4857bc8d2ed515ef67fa
Lyle Dodge <16616380+lyledodgegh@users.noreply.github.com>
2025-03-02 13:55:47 -0500

    added arc examples

---
+++ src/playground/arc/arc-enable-linux.sh
@@ -0,0 +1,24 @@
+# Add the service principal application ID and secret here
+servicePrincipalId="";
+servicePrincipalClientSecret="";
+
+export subscriptionId="";
+export resourceGroup="";
+export tenantId="";
+export location="";
+export authType="principal";
+export correlationId=$(uuidgen);
+export cloud="AzureCloud";
+
+LINUX_INSTALL_SCRIPT="/tmp/install_linux_azcmagent.sh"
+
+if [ -f "$LINUX_INSTALL_SCRIPT" ]; then rm -f "$LINUX_INSTALL_SCRIPT"; fi;
+output=$(wget https://gbl.his.arc.azure.com/azcmagent-linux -O "$LINUX_INSTALL_SCRIPT" 2>&1);
+
+if [ $? != 0 ]; then wget -qO- --method=PUT --body-data="{\"subscriptionId\":\"$subscriptionId\",\"resourceGroup\":\"$resourceGroup\",\"tenantId\":\"$tenantId\",\"location\":\"$location\",\"correlationId\":\"$correlationId\",\"authType\":\"$authType\",\"operation\":\"onboarding\",\"messageType\":\"DownloadScriptFailed\",\"message\":\"$output\"}" "https://gbl.his.arc.azure.com/log" &> /dev/null || true; fi;
+
+echo "$output";
+
+bash "$LINUX_INSTALL_SCRIPT";
+
+sudo azcmagent connect --service-principal-id "$ServicePrincipalId" --service-principal-secret "$ServicePrincipalClientSecret" --resource-group "$resourceGroup" --tenant-id "$tenantId" --location "$location" --subscription-id "$subscriptionId" --cloud "$cloud" --tags 'ArcSQLServerExtensionDeployment=Disabled' --correlation-id "$correlationId";
